dd5f4030e62e22b8f7ec9e731f63bf7bf914faf9
Shiv S <bmsahitya.assam@gmail.com>
2026-03-25 11:35:54 +0530

    Delete read directory

--- read/modules/ime/utils.js
+++
@@ -1,1 +0,0 @@
-export function phoneticTransform(e){if(!e)return"";let r=e;return r=r.replace(/O/g,"PROTECT_O"),r=r.replace(/o(?=[.,!?;।])/g,""),r=r.replace(/PROTECT_O/g,"o"),r=r.replace(/্+o/g,""),r.toLowerCase().trim()}
