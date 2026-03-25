e03f90d22dc8e0e9923655b21e76188ebf5c8d6f
Shiv S <bmsahitya.assam@gmail.com>
2026-03-25 11:19:21 +0530

    Add files via upload

---
+++ read/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'lexipic-v1';
+const ASSETS = [
+  '/aShiv/lexipic/',
+  '/aShiv/lexipic/manifest.json',
+  '/aShiv/lexipic/index.html',
+  '/aShiv/lexipic/style.css',
+  '/aShiv/lexipic/script.js',
+  '/aShiv/lexipic/database.js',
+  '/aShiv/lexipic/favicon.ico',
+  '/aShiv/lexipic/modules/ime/reverse.js',
+  '/aShiv/lexipic/modules/ime/reverseMap.js',
+  '/aShiv/lexipic/modules/ime/utils.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
